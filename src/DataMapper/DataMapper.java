ca770b3391fc10d74ff333e771b0bcca160d04be
damien sauvalle <damiensauvalle@gmail.com>
2017-11-09 14:59:01 +0100

    feat : ajout DataMapper & JoueurMapper basic

---
+++ src/DataMapper/DataMapper.java
@@ -0,0 +1,14 @@
+package DataMapper;
+
+public abstract class DataMapper<T> {
+
+    public DataMapper() {}
+
+    abstract T find(T o);
+
+    abstract void insert(T o);
+
+    abstract void delete(T o);
+
+    abstract void update(T o);
+}
