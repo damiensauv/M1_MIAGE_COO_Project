dc61ee871b359948e4c59c9d3d28876491a3bc48
damien sauvalle <damiensauvalle@gmail.com>
2017-12-08 16:35:49 +0100

    feat connextion Frame

--- src/main/java/View/ConnexionFrame.java
+++ src/main/java/View/ConnexionFrame.java
@@ -2,15 +2,17 @@ package View;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class ConnexionFrame extends JFrame {
+public class ConnexionFrame extends JFrame implements ActionListener {
 
     JButton login;
     TextField username;
     TextField password;
     JPanel panelCo;
 
-    public ConnexionFrame(){
+    public ConnexionFrame() {
 
         username = new TextField(25);
         password = new TextField(25);
@@ -18,13 +20,27 @@ public class ConnexionFrame extends JFrame {
 
         panelCo = new JPanel();
 
+        panelCo.add(new JLabel("username : "));
         panelCo.add(username);
+
+        panelCo.add(new JLabel("password : "));
         panelCo.add(password);
+
         panelCo.add(login);
+        login.addActionListener(this);
 
         this.setContentPane(panelCo);
-
-
     }
 
+    public void actionPerformed(ActionEvent e) {
+
+        if (e.getSource() == login){
+
+            // get username & password
+            // call Service
+            // Call Menu
+
+        }
+
+    }
 }
