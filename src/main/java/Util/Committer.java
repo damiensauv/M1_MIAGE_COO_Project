cdb092b60fbd6d63602d6114760d2927b58c00c2
damien sauvalle <damiensauvalle@gmail.com>
2017-12-12 20:10:36 +0100

    feat : add commiter for Game

--- src/main/java/Util/Committer.java
+++ src/main/java/Util/Committer.java
@@ -1,4 +1,19 @@
 package Util;
 
-public class Committer extends  Visitor {
+import Jeu.Interface.IGame;
+import Persistance.DataMapper.GameMapper;
+
+import java.sql.SQLException;
+
+public class Committer extends Visitor {
+
+    public void visiter(IGame o) {
+        try {
+
+            GameMapper.getInstance().update(o);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
