bf905da4809a7bab0527e324ffe3f605cd2fec7c
damien sauvalle <damiensauvalle@gmail.com>
2017-12-14 22:44:26 +0100

    Feat : prepare Carte for persistance

--- src/main/java/Jeu/Entity/Carte.java
+++ src/main/java/Jeu/Entity/Carte.java
@@ -1,93 +1,110 @@
 package Jeu.Entity;
 
+import Jeu.Interface.ICarte;
+import Util.Observer;
+import Util.Visitor;
+
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
+public class Carte extends AObject implements ICarte {
 
-public class Carte {
+    private Territoire[][] carte;
+    private List<Ville> listeVilles;
+    private List<Armee> listeArmee;
 
-	private Territoire[][] carte;
-	ArrayList<Ville> listeVilles;
-	ArrayList<Armee> listeArmee;
+    // TODO : revoir le constuctor
+    public Carte(int sizeX, int sizeY) {
+        int randomInt;
 
-	// On déclare la carte avec ses dimensions
+        this.listeArmee = new ArrayList<Armee>();
+        this.listeVilles = new ArrayList<Ville>();
 
-	// possede un Id
+        this.carte = new Territoire[sizeX][sizeY];
 
-	public Carte(int x, int y) {
+        // Il nous faut un nombre aléatoire pour générer les cases de la map
+        Random randomGenerator = new Random();
 
-		super();
-		this.carte = new Territoire[x][y];
+        // On crée la carte ligne par ligne
+        for (int i = 0; i < sizeY; i++) {
+            for (int j = 0; j < sizeX; j++) {
+                randomInt = randomGenerator.nextInt(3);
+                this.setTerritoire(new Coordonnees(i, j), randomInt);
+            }
+        }
+    }
 
-		// Il nous faut un nombre aléatoire pour générer les cases de la map
-		Random randomGenerator = new Random();
-		int randomInt;
+    public void setTerritoire(Coordonnees coord, int type) {
+        int xpos = coord.getX();
+        int ypos = coord.getY();
 
-		// On crée la carte ligne par ligne
-		for (int i = 0; i < y; i++) {
-			for (int j = 0; j < x; j++) {
-				randomInt = randomGenerator.nextInt(3);
-				this.setTerritoire(new Coordonnees(i, j), randomInt);
-			}
-		}
-	}
+        switch (type) {
+            case 0:
+                this.carte[xpos][ypos] = new Plaine(coord);
+                break;
+            case 1:
+                this.carte[xpos][ypos] = new Champ(coord);
+                break;
+            case 2:
+                this.carte[xpos][ypos] = new Montagne(coord);
+                break;
+            default:
+                break;
+        }
+    }
 
-	public void setTerritoire(Coordonnees coord, int type) {
+    public Territoire[][] getCarte() {
+        return carte;
+    }
 
-		int xpos = coord.getX() ;
-		int ypos = coord.getY() ;
-		switch (type) {
-		case 0:
-			this.carte[xpos][ypos] = new Plaine(coord);
-			break;
-		case 1:
-			this.carte[xpos][ypos] = new Champ(coord);
-			break;
-		case 2:
-			this.carte[xpos][ypos] = new Montagne(coord);
-			break;
+    public void setCarte(Territoire[][] carte) {
+        this.carte = carte;
+    }
 
-		default:
-			break;
-		}
+    public List<Ville> getListeVilles() {
+        return listeVilles;
+    }
 
-	}
+    public void addVille(Ville ville) {
+        this.listeVilles.add(ville);
+    }
 
-	public Territoire[][] getCarte() {
-		return carte;
-	}
+    public void setListeVilles(List<Ville> listeVilles) {
+        this.listeVilles = listeVilles;
+    }
 
-	public void setCarte(Territoire[][] carte) {
-		this.carte = carte;
-	}
+    public List<Armee> getListeArmee() {
+        return listeArmee;
+    }
 
-	public ArrayList<Ville> getListeVilles() {
-		return listeVilles;
-	}
+    public void addArmee(Armee armee) {
+        this.listeArmee.add(armee);
+    }
 
-	public void setListeVilles(ArrayList<Ville> listeVilles) {
-		this.listeVilles = listeVilles;
-	}
+    public void setListeArmee(List<Armee> listeArmee) {
+        this.listeArmee = listeArmee;
+    }
 
-	public ArrayList<Armee> getListeArmee() {
-		return listeArmee;
-	}
+    public void AfficherCarteDebug() {
+        for (int i = 0; i < this.carte.length; i++) {
+            for (int j = 0; j < this.carte[i].length; j++) {
+                this.carte[i][j].AfficherTypeTerritoire();
+            }
+            System.out.println("");
+        }
+    }
 
-	public void setListeArmee(ArrayList<Armee> listeArmee) {
-		this.listeArmee = listeArmee;
-	}
+    public void add(Observer o) {
+        obs.add(o);
+    }
 
-	public void AfficherCarteDebug() {
+    public void notifier() {
+        for (Observer o : obs)
+            o.action(this);
+    }
 
-		for (int i = 0; i < this.carte.length; i++) {
-			for (int j = 0; j < this.carte[i].length; j++) {
-
-				this.carte[i][j].AfficherTypeTerritoire();
-
-			}
-			// A la ligne
-			System.out.println("");
-		}
-
-	}
+    public void accept(Visitor v) {
+        v.visiter(this);
+    }
 }
--- src/main/java/Jeu/Interface/ICarte.java
+++ src/main/java/Jeu/Interface/ICarte.java
@@ -1,6 +1,32 @@
 package Jeu.Interface;
 
+import Jeu.Entity.Armee;
+import Jeu.Entity.Coordonnees;
+import Jeu.Entity.Territoire;
+import Jeu.Entity.Ville;
 import Util.IDomainObject;
 
+import java.util.List;
+
 public interface ICarte extends IDomainObject {
+
+    void setTerritoire(Coordonnees coord, int type);
+
+    Territoire[][] getCarte();
+
+    void setCarte(Territoire[][] carte);
+
+    List<Ville> getListeVilles();
+
+    void addVille(Ville ville);
+
+    void setListeVilles(List<Ville> listeVilles);
+
+    List<Armee> getListeArmee();
+
+    void addArmee(Armee armee);
+
+    void setListeArmee(List<Armee> listeArmee);
+
+    void AfficherCarteDebug();
 }
