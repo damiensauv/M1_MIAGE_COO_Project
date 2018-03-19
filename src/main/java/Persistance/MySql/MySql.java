a44bf21189640d5f4ed338c625a8c20068bad0aa
damien sauvalle <damiensauvalle@gmail.com>
2018-03-19 14:14:07 +0100

    Update Mysql
    update ********

--- src/main/java/Persistance/MySql/MySql.java
+++ src/main/java/Persistance/MySql/MySql.java
@@ -21,7 +21,7 @@ public class MySql {
     // connection a la base MySql
     private void initConnexion() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
-        connection = DriverManager.getConnection("jdbc:mysql://webtp.fil.univ-lille1.fr/sauvalle", "sauvalle", "toto62");
+        connection = DriverManager.getConnection("jdbc:mysql://webtp.fil.univ-lille1.fr/sauvalle", "sauvalle", "*****");
     }
 
     public static Connection getInstance() {
